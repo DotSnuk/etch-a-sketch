0ace7711e10b38f08ac8c8f65b851f705e370643
DotSnuk <hampusfarner@outlook.com>
2023-06-06 12:26:02 +0200

    addDiv, addRow, createGrid functions added. grid displaying correctly. a little 'hacky' way of not getting the css borders to overlap

---
+++ script.js
@@ -0,0 +1,23 @@
+function addDiv(){
+    const newDiv = document.createElement('div');
+    const rowContainer = document.querySelector('.row:last-child');
+    newDiv.className = 'grid';
+    rowContainer.appendChild(newDiv);
+}
+
+function addRow(){
+    const newDiv = document.createElement('div');
+    const gridContainer = document.querySelector('#div-container');
+    newDiv.className = 'row';
+    gridContainer.appendChild(newDiv);
+}
+
+function createGrid(){
+    for (let i = 0; i < 16; i++){
+        addRow();
+        for (let i = 0; i < 16; i++){
+            addDiv();
+        }
+    }
+}
+createGrid();
