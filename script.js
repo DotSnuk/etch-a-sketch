783bdae50bddbabe263fff185a59bcf118f61e5f
DotSnuk <hampusfarner@outlook.com>
2023-06-06 20:25:09 +0200

    only one grid trailing cursor, why?

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function changeColor(){
         const currentColor = d.style.background;
         const values = currentColor.substring(4, (currentColor.length - 1)).split(',');
         console.log(values);
-        d.target.style.background = ("rgb(" + (values[0] + 30) + ", "
+        d.style.background = ("rgb(" + (values[0] + 30) + ", "
                                         + (values[1] + 30) + ", "
                                         + (values[2] + 30) + ")");
                                         // look at this, doesnt work
